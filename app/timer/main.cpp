b49c3344f2b48ce53a04865bbd70131c34516d89
DragonCoder01 <dragon-coder@hotmail.com>
2020-05-29 20:39:59 +0200

    moved implementation from zol.h to zol.cpp

--- app/timer/main.cpp
+++ app/timer/main.cpp
@@ -1,15 +1,6 @@
-// The function `loop` gets tied to the `ctc_timer0_isr`
-#define ZOL_USE_TIMER0_LOOP
-
 #include "zol/zol.h"
 
-void setup() {
-	// initializes timer0 to provide a 1ms timer interrupt
-	ctc_timer0_1ms;
-	digitalPin13.output();
-}
-
-void loop() {
+ISR(TIMER0_COMPA_vect) {
 	static int i = 0;
 	++i;
 	if (i >= 500) {
@@ -17,3 +8,11 @@ void loop() {
 		digitalPin13.toggle();
 	}
 }
+
+void setup() {
+	// initializes timer0 to provide a 1ms timer interrupt
+	ctc_timer0_1ms;
+	digitalPin13.output();
+}
+
+void loop() {}
