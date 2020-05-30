dfc484b2a66b2d1b673e53dfb0b5f75d35129005
DragonCoder01 <dragon-coder@hotmail.com>
2020-05-30 12:43:49 +0200

    Made register.h more c++ like

--- app/register/main.cpp
+++ app/register/main.cpp
@@ -2,12 +2,11 @@
 
 #include <util/delay.h>
 
-
 void setup() {
-	Register<uint8_t>{ DDRB }.set_bit(5);
+	Register<uint8_t, 0x24>{}.set_bit(5);
 }
 
 void loop() {
-	Register<uint8_t>{ PINB }.set_bit(5);
+	Register<uint8_t, 0x23>{}.set_bit(5);
 	_delay_ms(500);
 }
--- lib/zol/util/register.h
+++ lib/zol/util/register.h
@@ -1,19 +1,54 @@
 #pragma once
 
-template<typename type>
+template<typename type, int address>
 class Register {
 public:
-	volatile type& reg;
+	/// @brief Sets the `bit_number`th bit in the register.
+	/// @param bit_number ... Bit to set
+	inline void set_bit(const int bit_number) {
+		*reinterpret_cast<volatile type*>(address) |= (1 << bit_number);
+	}
 
-	inline void set_bit(const int bit_number) { reg |= (1 << bit_number); }
-	inline void clear_bit(const int bit_number) { reg &= ~(1 << bit_number); }
-	inline void toggle_bit(const int bit_number) { reg ^= (1 << bit_number); }
+	/// @brief Clears the `bit_number`th bit in the register.
+	/// @param bit_number ... Bit to clear
+	inline void clear_bit(const int bit_number) {
+		*reinterpret_cast<volatile type*>(address) &= ~(1 << bit_number);
+	}
 
-	inline void set_reg(const type content) { reg = content; }
-	inline void clear_reg() { reg = 0; }
-	inline void or_reg(const type content) { reg |= content; }
-	inline void and_reg(const type content) { reg &= content; }
-	inline void toggle_reg(const type content) { reg ^= content; }
+	/// @brief Toggles the `bit_number`th bit in the register.
+	/// @param bit_number ... Bit to toggle
+	inline void toggle_bit(const int bit_number) {
+		*reinterpret_cast<volatile type*>(address) ^= (1 << bit_number);
+	}
 
-	Register(volatile type& address) : reg{ *(volatile type*) (&address) } {}
+	/// @brief Sets the register to `content`.
+	/// @param content ... The new value of the register.
+	inline void set_reg(const type content) {
+		*reinterpret_cast<volatile type*>(address) = content;
+	}
+
+	/// @brief Sets the register to 0.
+	inline void clear_reg() {
+		*reinterpret_cast<volatile type*>(address) = 0;	   //
+	}
+
+	/// @brief Ors the register with `bitmask`.
+	/// @param bitmask ... It's a bitmask, duh.
+	inline void or_reg(const type bitmask) {
+		*reinterpret_cast<volatile type*>(address) |= bitmask;
+	}
+
+	/// @brief Ands the register with `bitmask`.
+	/// @param bitmask ... It's a bitmask, duh
+	inline void and_reg(const type bitmask) {
+		*reinterpret_cast<volatile type*>(address) &= bitmask;
+	}
+
+	/// @brief Toggles (XOrs) the register with `bitmask`.
+	/// @param bitmask ... It's a bitmask, duh
+	inline void toggle_reg(const type bitmask) {
+		*reinterpret_cast<volatile type*>(address) ^= bitmask;
+	}
+
+	Register() {}
 };
